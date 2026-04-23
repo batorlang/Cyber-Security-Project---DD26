a37b0dc0ad380f7744777b1e519b28de937573a0
Márton Zsombor Magyar <Marton.Magyar@student.lut.fi>
2026-04-23 15:04:14 +0300

    added logout button

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,17 +1,29 @@
 import React from 'react'
-import { Grid } from '@mui/material'
+import { useNavigate, Link } from 'react-router-dom'
+import { Button, Grid } from '@mui/material'
 import Profiles from '../components/Profiles'
 import Chat from '../components/Chat'
 import '../styles/Home.css'
 
 
 const Home = () => {
+    const navigate = useNavigate()
 
+    const handleLogout = () => {
+        // Implement logout logic here
+        console.log('User logged out')
+        navigate('/login')
+    }
+    
 
     return (
         <div className="page-wrapper">
             <nav>
                 <div className="title">Chats</div>
+                <Button variant="contained" className="logoutbtn" onClick={handleLogout}>
+                    Log out
+                </Button>
+
             </nav>
             <Grid container className="main-content">
                 <Grid item xs={4} className="main-column">
